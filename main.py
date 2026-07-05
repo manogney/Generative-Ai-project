a0c91a05b1a4b55bb9c86e0d698962d555220b41
manogney <manogney444@gmail.com>
2026-07-06 01:25:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# import streamlit as st
+# st.title(" 📧 Cold-email-generator")
+# url_input=st.text_input("Enter a URL:",value="https://careers.nike.com/software-engineer/job/R-82026")
+# submit_button=st.button("Submit")
+#
+# if submit_button:
+#     st.code("Hello Hiring Manager,I am from AtliQ",language='markdown')
+# Fix: "No module named 'bs4'"
+# Easy fix! Just install BeautifulSoup in Command Prompt:
+# pip install beautifulsoup4
+#
+# Then restart your app and try submitting the URL again. ✅
+# bs4 is BeautifulSoup — it's used to read and scrape webpage content from the job URL you entered.
+
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
+
+
+def create_streamlit_app(llm, portfolio, clean_text):
+    st.title("📧 Cold Mail Generator")
+    url_input = st.text_input("Enter a URL:", value="https://www.google.com/about/careers/applications/jobs/results?q=&hl=en")
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs:
+                skills = job.get('skills', [])
+                links = portfolio.query_links(skills)
+                email = llm.write_mail(job, links)
+                st.code(email, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+# main function
+if __name__ == "__main__":
+    chain = Chain()
+    portfolio = Portfolio()
+    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    # call streamlit_app function
+    create_streamlit_app(chain, portfolio, clean_text)
+
